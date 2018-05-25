b20fbf9b0cc75a5e60f18c12bdad1597b455e6c3
aless <aless@10.255.50.20>
2018-05-25 16:23:39 +0200

    Added some rules

--- sonar-wekanose-plugin/src/main/java/it/unimib/disco/essere/WekaNosePlugin/entrypoint/RulesList.java
+++ sonar-wekanose-plugin/src/main/java/it/unimib/disco/essere/WekaNosePlugin/entrypoint/RulesList.java
@@ -9,6 +9,18 @@ import it.unimib.disco.essere.WekaNosePlugin.checks.Rule05;
 import it.unimib.disco.essere.WekaNosePlugin.checks.Rule06;
 import it.unimib.disco.essere.WekaNosePlugin.checks.Rule07;
 import it.unimib.disco.essere.WekaNosePlugin.checks.Rule08;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule09;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule10;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule11;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule12;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule13;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule14;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule15;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule16;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule17;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule18;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule19;
+import it.unimib.disco.essere.WekaNosePlugin.checks.Rule20;
 
 import java.util.List;
 import org.sonar.plugins.java.api.JavaCheck;
@@ -28,6 +40,9 @@ public final class RulesList {
 	public static List<Class<? extends JavaCheck>> getJavaChecks() {
 
 		return ImmutableList.<Class<? extends JavaCheck>>builder().add(Rule01.class).add(Rule02.class).add(Rule03.class)
-				.add(Rule04.class).add(Rule05.class).add(Rule06.class).add(Rule07.class).add(Rule08.class).build();
+				.add(Rule04.class).add(Rule05.class).add(Rule06.class).add(Rule07.class).add(Rule08.class)
+				.add(Rule09.class).add(Rule10.class).add(Rule11.class).add(Rule12.class).add(Rule13.class)
+				.add(Rule14.class).add(Rule15.class).add(Rule16.class).add(Rule17.class).add(Rule18.class)
+				.add(Rule19.class).add(Rule20.class).build();
 	}
 }
